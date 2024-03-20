8823086ba2782acf194abbadb1ffcbb29e27e1ff
MACRODAT <macrodatservices@gmail.com>
2024-03-20 02:06:23 +0000

    Changes to the file

--- 0x14-mysql/cd.pp
+++ 0x14-mysql/cd.pp
@@ -1,10 +1,18 @@
-package { 'mysql-server-5.7':
-  ensure => "5.7.x",
+# Define class for MySQL installation and management
+class mysql {
+    # Install MySQL server version 5.7.x
+    package { 'mysql-server':
+        ensure => '5.7.1',
+    }
+
+    # Start and enable MySQL service
+    service { 'mysql':
+        ensure => 'running',
+        enable => true,
+        require => Package['mysql-server'],
+    }
 }
 
-service { 'mysql':
-  ensure => running,
-  enable => true,
-  require => Package['mysql-server-5.7']
-}
+# Include the mysql class
+include mysql
 
