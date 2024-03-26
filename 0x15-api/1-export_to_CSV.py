a6dfcec704925b729c00e3f1b4e33dfc6d961257
MACRODAT <macrodatservices@gmail.com>
2024-03-26 02:58:33 +0000

    Changes to the file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""Exports to-do list infos
+for a given employee
+to CSV format."""
+
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+path_url = "https://jsonplaceholder.typicode.com/"
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+
+if __name__ == "__main__":
+    usr_id = sys.argv[1]
+    user = requests.get(path_url + "users/{}".format(usr_id)).json()
+    username = user.get("username")
+    todos = requests.get(path_url + "todos", params={"userId": usr_id}).json()
+
+    with open("{}.csv".format(usr_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
+            [usr_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
