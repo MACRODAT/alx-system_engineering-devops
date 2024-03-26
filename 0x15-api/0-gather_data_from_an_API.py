9bd139cb955ffbcdf2aa53b9bcde34ea5e88580e
MACRODAT <macrodatservices@gmail.com>
2024-03-26 02:52:50 +0000

    Changes to the file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,17 +4,18 @@
 Script to fetch data from API
 """
 
-import requests, urllib
+import requests
 from sys import argv
 
 REST_API = "https://jsonplaceholder.typicode.com"
+
 
 if __name__ == "__main__":
     employer = argv[1]
     j = requests.get(f'{REST_API}/users/{argv[1]}').json()
     tasks = requests.get(f'{REST_API}/todos').json()
     name = j.get('name')
-    alltasks = list(filter(lambda x: x.get('userId' == argv[1], tasks))
+    alltasks = list(filter(lambda x: x.get('userId' == argv[1]), tasks))
     completed_tasks = list(filter(lambda x: x.get('completed'), alltasks))
     print(
         'Employee {} is done with tasks({}/{}):'.format(
