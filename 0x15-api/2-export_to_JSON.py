9198af86904ffadb7fa816b9dad089feaed31f07
MACRODAT <macrodatservices@gmail.com>
2024-03-26 03:02:30 +0000

    Changes to the file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+"""Exports to-do list information for emp
+in json format"""
+
 import json
 import requests
 import sys
 
+url = "https://jsonplaceholder.typicode.com/"
+
+
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
